0546087ab45b88865e44fdce67fe4d6a900077a7
bunderontheway <bunderontheway@gmail.com>
2021-07-22 08:01:29 +0700

    gocar

---
+++ practice oop - (simple gojek)/GoCar.java
@@ -0,0 +1,14 @@
+class GoCar extends GoRide {
+    
+    
+    GoCar(String nama, String lokasi, String alamat_tujuan) {
+        super(nama, lokasi, alamat_tujuan);
+    }
+    
+    
+    void pesananAnda() {
+        System.out.println("Halo " + nama + ", anda ingin pergi dengan GO CAR dari " + lokasi + " menuju " + alamat_tujuan);
+        //System.out.println("Harga yang anda perlukan adalah " + super.harga_gocar);
+    }
+    
+}
