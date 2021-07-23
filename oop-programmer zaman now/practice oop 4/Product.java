dc6cdfaa9e3845e7a263ab9fa20312928de55e8c
bunderontheway <bunderontheway@gmail.com>
2021-07-24 00:19:07 +0700

    practice

---
+++ oop-programmer zaman now/practice oop 4/Product.java
@@ -0,0 +1,11 @@
+public class Product {
+    
+    protected String name;
+    protected int price;
+    
+    Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+    
+}
